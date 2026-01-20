73cb933c51cb43df0f0667407aa3c40688c667ff
White-Frost-G <116251777+White-Frost-G@users.noreply.github.com>
2026-01-20 12:23:57 +0200

    Added icons and select

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('paymentChoice');
+export class App implements OnInit {
+  items = [
+    { name: 'Body', image: '/body.png',},
+    { name: 'Visa', image: '/visa.png' },
+    { name: 'Mastercard', image: '/mastercard.png' },
+    { name: 'PayPal', image: '/paypal.png' }
+  ];
+  selectedItem = this.items[0];
+  canChange: boolean = true;
+
+  selectItem(item: any){
+    if (this.canChange) return;
+    this.selectedItem = item;
+  }
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    this.route.queryParams.subscribe(params => {
+      const canChangeChoice = params['change'];
+      this.canChange = canChangeChoice == "false";
+    });
+  }
 }
